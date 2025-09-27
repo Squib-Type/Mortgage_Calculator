e2aab79955f64c57a6601d288bb6246e1f4a8c50
Squib-Type <kjohnson133922@gmail.com>
2025-09-27 18:35:52 -0500

    Exercise 2 Complete

--- app/src/main/java/com/example/mortgage_calculator/MainActivity.kt
+++ app/src/main/java/com/example/mortgage_calculator/MainActivity.kt
@@ -3,17 +3,11 @@ package com.example.mortgage_calculator
 import android.app.Activity
 import android.content.Intent
 import android.os.Bundle
-import android.widget.Button
-import android.widget.EditText
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
+
 import androidx.activity.result.contract.ActivityResultContracts
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
+
 import com.example.mortgage_calculator.databinding.ActivityMainBinding
-
-
 
 
 class MainActivity : AppCompatActivity() {
@@ -31,12 +25,6 @@ class MainActivity : AppCompatActivity() {
             }
     }
     private lateinit var binding: ActivityMainBinding
-    private lateinit var editPrincipal: EditText
-    private lateinit var editInterestRate: EditText
-    private lateinit var editLoanTerm: EditText
-    private lateinit var calculateButton: Button
-    private lateinit var paymentsResults: TextView
-
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
